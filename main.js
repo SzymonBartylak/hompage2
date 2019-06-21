3cc4754c506d12ec8441970f29bd3b228566bd8b
Szymon Bartylak <bartylak.szymon@gmail.com>
2019-06-21 19:23:23 +0200

    box training

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('hello there');
+const name = 'Szymon Bartylak' ;
+const age = 21 ;
+console.log(name);
+console.log(age);
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+const mainHeader = document.querySelector('.mainHeader')
+console.log(mainHeader.innerHTML);
+
+mainHeader.innerHTML = "lolczik zwałczki technolodżik"
